c26ab1c75f0a02a75862ae3f26bffeb823d0311b
aavanesy <aavanesy@outlook.com>
2020-07-23 22:40:54 +0400

    1 initial push

---
+++ demo.R
@@ -0,0 +1,12 @@
+
+#this is a demo project to test renv
+#using main packages at this point
+
+library(renv)
+#renv::init()
+
+options(renv.download.override = utils::download.file)
+library(tidyverse)
+library(officer)
+library(flextable)
+library(openxlsx)
